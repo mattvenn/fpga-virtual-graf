e4c849fd7237f34948ef437d150aa56a3ef70403
matt venn <matt@mattvenn.net>
2016-10-28 16:21:42 +0200

    sending single packet on button press

--- i2c.v
+++ i2c.v
@@ -13,10 +13,25 @@ module top (
     input button,
 );
     
-    reg i2c_clk = 0;
+    localparam STATE_IDLE = 0;
+    localparam STATE_PK1 = 1;
+    localparam STATE_WAIT1 = 2;
+    localparam STATE_PK2 = 3;
+    localparam STATE_WAIT2 = 4;
+    localparam STATE_STOP = 5;
 
-    i2c_master i2c(.clk (button_clk), .reset (edge_out), .i2c_sda (i2c_sda), .i2c_scl (i2c_scl), .out(outcount));
-    debounce db1(.clk (button_clk), .button (button), .debounced (edge_out));
+    reg dbutton;
+    reg [6:0] i2c_addr;
+    reg [7:0] i2c_data;
+    reg reset = 1;
+    reg i2c_start = 0;
+	reg [8:0] counter = 0;
+    reg [4:0] outcount;
+    reg [4:0] state = STATE_IDLE;
+    reg start = 0;
+
+    i2c_master i2c(.clk (button_clk), .start(i2c_start), .addr(i2c_addr), .data(i2c_data), .reset (reset), .i2c_sda (i2c_sda), .i2c_scl (i2c_scl), .out(outcount), .ready(i2c_ready));
+    debounce db1(.clk (button_clk), .button (button), .debounced (dbutton));
 
     SB_PLL40_CORE #(
         .FEEDBACK_PATH("SIMPLE"),
@@ -33,16 +48,67 @@ module top (
         .PLLOUTCORE(pixartclk)
     );
 
-    reg reset;
-    reg firstreset = 0;
+	assign {LED1, LED2, LED3, LED4, LED5} = state;
+/*
+assign LED1 = i2c_start;
+    assign LED2 = dbutton;
+    assign LED3 = i2c_ready;
+    assign LED4 = reset;
+    */
+    assign edge_out = dbutton;
 
-    initial begin
-        reset = 1;
+    // can only assign reg in one always block
+    always@(dbutton) begin
+        if(dbutton)
+            start = 0;
+        else
+            start = 1;
     end
 
-	reg [8:0] counter = 0;
-    reg [4:0] outcount;
-	assign {LED1, LED2, LED3, LED4, LED5} = outcount;
+    always@(posedge clk) begin
+        case(state)
+            STATE_IDLE: begin
+                if(start) begin
+                    state <= STATE_PK2;
+                end
+            end
+            STATE_PK1: begin
+                i2c_addr <= 7'h20;
+                i2c_data <= 8'hAA;
+                i2c_start <= 1;
+                state <= STATE_WAIT1;
+            end
+            STATE_WAIT1: begin
+                if(i2c_ready == 0) begin
+                    i2c_start <= 0;
+                    state <= STATE_PK2;
+                end
+                else begin
+                    state <= STATE_WAIT1;
+                end
+            end
+            STATE_PK2: begin
+                i2c_addr <= 7'h20;
+                i2c_data <= 8'hAB;
+                i2c_start <= 1;
+                state <= STATE_WAIT2;
+            end
+            STATE_WAIT2: begin
+                if(i2c_ready == 0) begin
+                    i2c_start <= 0;
+                    state <= STATE_STOP;
+                end
+                else begin
+                    state <= STATE_WAIT2;
+                end
+            end
+            STATE_STOP: begin
+                // wait till trigger is finished
+                if(start == 0)
+                    state <= STATE_IDLE;
+            end
+        endcase
+    end
 
 	always@(posedge clk) begin
         reset <= 0;
@@ -50,26 +116,20 @@ module top (
         if(counter == 0)
             button_clk <= ~ button_clk;
 	end
-/*
-        if(outcnt == 1) begin
-            if(reset == 0 && firstreset == 0) begin
-                reset = 1;
-                firstreset = 1;
-                end
-            else
-                reset = 0;
-        end
-	end
-*/
 
 endmodule
+
 
 module i2c_master(
     input wire clk,
     input wire reset,
+    input wire start,
+    input wire [6:0] addr,
+    input wire [7:0] data,
     output reg i2c_sda,
     output wire i2c_scl,
     output reg [4:0] out,
+    output wire ready
 );
     localparam STATE_IDLE = 0; // no clock
     localparam STATE_START = 1;// no clock
@@ -81,17 +141,18 @@ module i2c_master(
     localparam STATE_STOP = 7; // no clock
 
     reg [8:0] state;
-    reg [6:0] addr;
     reg [7:0] count;
-    reg [7:0] data;
     reg i2c_scl_enable = 0;
+
+    reg [6:0] saved_addr;
+    reg [7:0] saved_data;
 
     initial begin
         state = STATE_IDLE;
     end
 
     assign out = state;
-
+    assign ready = (reset == 0) && (state == STATE_IDLE) ? 1 : 0;
     assign i2c_scl = (i2c_scl_enable == 0) ? 1 : ~clk;
     
     always@(negedge clk) begin
@@ -109,26 +170,28 @@ module i2c_master(
 
 	always@(posedge clk) begin
         if( reset == 1 ) begin
-            data <= data + 1;
             state <= STATE_IDLE;
             i2c_sda <= 1;
-    //        i2c_scl <= 1;
-            addr <= 7'h40;
             count <= 8'd0;
         end
         else begin
             case(state)
                 STATE_IDLE: begin
                     i2c_sda <= 1;
-                    state <= STATE_START;
+                    if (start) begin
+                        saved_addr <= addr;
+                        saved_data <= data;
+                        state <= STATE_START;
+                    end
+                    else state <= STATE_IDLE;
                 end
                 STATE_START: begin
                     i2c_sda <= 0;
                     state <= STATE_ADDR;
                     count <= 6;
                 end
-                STATE_ADDR: begin // send address 7 bits, MSB
-                    i2c_sda <= addr[count];
+                STATE_ADDR: begin // send saved_address 7 bits, MSB
+                    i2c_sda <= saved_addr[count];
                     if(count == 0) state <= STATE_RW;
                     else count <= count - 1;
                 end
@@ -138,23 +201,23 @@ module i2c_master(
                 end
                 STATE_WACK: begin
                     state <= STATE_DATA;
-                    // need to read this data
+                    // need to read this saved_data
                     i2c_sda <= 0;
                     count <= 7;
                 end
                 STATE_DATA: begin
-                    i2c_sda <= data[count];
+                    i2c_sda <= saved_data[count];
                     if(count == 0) state <= STATE_WACK2;
                     else count <= count - 1;
                 end
                 STATE_WACK2: begin
-                    // need to read this data
+                    // need to read this saved_data
                     i2c_sda <= 0;
                     state <= STATE_STOP;
                 end
                 STATE_STOP: begin
                     i2c_sda <= 1;
-//                    state <= STATE_START;
+                    state <= STATE_IDLE;
                 end
             endcase
         end
