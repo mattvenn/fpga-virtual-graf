1959a85e47a306d68a2b7c2fd03f91189ace12eb
matt venn <matt@mattvenn.net>
2016-11-06 09:38:22 +0100

    tristate sda

--- i2c_master.v
+++ i2c_master.v
@@ -6,7 +6,7 @@ module i2c_master(
     input wire [6:0] addr,
     input wire [7:0] data,
     input wire start,
-    output reg i2c_sda,
+    output wire i2c_sda,
     output wire i2c_scl,
     output wire ready
 );
@@ -21,6 +21,7 @@ module i2c_master(
 
     reg [8:0] state;
     reg [7:0] count;
+    reg i2c_sda_tri;
     reg i2c_scl_enable = 0;
 
     reg [6:0] saved_addr;
@@ -32,6 +33,7 @@ module i2c_master(
 
     assign ready = (reset == 0) && (state == STATE_STOP || state == STATE_IDLE) ? 1 : 0;
     assign i2c_scl = (i2c_scl_enable == 0) ? 1 : ~clk;
+    assign i2c_sda = (i2c_sda_tri == 1) ? 'bz : 0;
     
     always@(negedge clk) begin
         if( reset == 1 ) begin
@@ -49,13 +51,13 @@ module i2c_master(
 	always@(posedge clk) begin
         if( reset == 1 ) begin
             state <= STATE_IDLE;
-            i2c_sda <= 1;
+            i2c_sda_tri <= 'b1;
             count <= 8'd0;
         end
         else begin
             case(state)
                 STATE_IDLE: begin
-                    i2c_sda <= 1;
+                    i2c_sda_tri <= 1;
                     if (start) begin
                         saved_addr <= addr;
                         saved_data <= data;
@@ -64,38 +66,38 @@ module i2c_master(
                     else state <= STATE_IDLE;
                 end
                 STATE_START: begin
-                    i2c_sda <= 0;
+                    i2c_sda_tri <= 0;
                     state <= STATE_ADDR;
                     count <= 6;
                 end
                 STATE_ADDR: begin // send saved_address 7 bits, MSB
-                    i2c_sda <= saved_addr[count];
+                    i2c_sda_tri <= saved_addr[count];
                     if(count == 0) state <= STATE_RW;
                     else count <= count - 1;
                 end
                 STATE_RW: begin
-                    i2c_sda <= 1;
+                    i2c_sda_tri <= 1;
                     state <= STATE_WACK;
                 end
                 STATE_WACK: begin
                     state <= STATE_DATA;
                     // need to read this saved_data
-                    i2c_sda <= 0;
+                    i2c_sda_tri <= 1;
                     count <= 7;
                 end
                 STATE_DATA: begin
-                    i2c_sda <= saved_data[count];
+                    i2c_sda_tri <= saved_data[count];
                     if(count == 0) state <= STATE_WACK2;
                     else count <= count - 1;
                 end
                 STATE_WACK2: begin
                     // need to read this saved_data
-                    i2c_sda <= 0;
+                    i2c_sda_tri <= 1;
                     state <= STATE_STOP;
                 end
                 //stay in stop till reset
                 STATE_STOP: begin
-                    i2c_sda <= 1;
+                    i2c_sda_tri <= 1;
                     state <= STATE_IDLE;
                 end
             endcase
