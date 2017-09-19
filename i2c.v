1a0b46b9b8dbfc52260a8235d472b40d3b1fcbe8
matt venn <matt@mattvenn.net>
2017-09-19 17:43:39 +0200

    added button board

--- i2c.v
+++ i2c.v
@@ -8,11 +8,14 @@ module top (
 	input  clk,
 	output [0:3] LED,
     input [0:1] BUTTON,
+    input reset_button,
+    input erase_button,
+    output status_led,
+    output cam_led,
     output pixartclk,
     output pixart_reset,
     inout i2c_sda,
     output i2c_scl,
-    output i2c_sda_dir,
     `ifdef sram
     output [17:0] ADR,
     inout [15:0] DAT,
@@ -23,16 +26,29 @@ module top (
     output [31:0] PMOD
 );
     
-    // synchronous reset button
+
+    // flashing status light
     always @(posedge clk) begin
-        // button normally high, press makes line go low
-        reset <= ~BUTTON[1];
-        led_counter <= led_counter + 1;
+        if(reset)
+            led_counter <= 0;
+        else
+            led_counter <= led_counter + 1;
     end
 
+    // led assignments
     assign LED[0] = reset;
     assign LED[1] = cam_valid;
     assign LED[2] = led_counter[26]; // flash slowly
+    assign cam_led = LED[1]; // cam valid repeat
+    assign status_led = LED[2]; // system flash repeat
+
+    // button wires
+    wire erase;
+    wire reset;
+
+    assign erase = ~BUTTON[0] | ~erase_button;
+    assign reset = ~BUTTON[1] | ~reset_button;
+
     reg [26:0] led_counter;
 
     wire cam_valid;
@@ -45,8 +61,8 @@ module top (
     reg[9:0] x;
     reg[9:0] y; // could be [8:0]
 
-    wire reset;
-    assign pixart_reset = 1;
+    // pixart clock is 25MHz, reset low to reset
+    assign pixart_reset = !reset;
     assign pixartclk = vga_clk;
 
     wire i2c_sda_out;
@@ -64,6 +80,8 @@ module top (
    `ifdef xyleds
    xy_leds leds(.reset(reset), .x(x), .y(y), .LED1(LED[0]), .LED2(LED[1]),.LED3(LED[2]),.LED4(LED[3]));
    `endif
+
+   wire i2c_sda_dir;
 
     SB_IO #(
          .PIN_TYPE(6'b 1010_01),
@@ -149,30 +167,25 @@ module top (
         .address_pins(ADR), 
         .OE(RAMOE), .WE(RAMWE), .CS(RAMCS)
         );
-    
-   // pixel_buffer pb(.clk(vga_clk), .reset(reset), .erase_button(~BUTTON[0]), .ram_state(PMOD[23:20]), .address(address), .data_read(data_read), .data_write(data_write), .read(read), .write(write), .ready(ready), .pixels(pixels), .hcounter(hcounter), .vcounter(vcounter), .x(x), .y(y));
+
+   // pixel buffer reads video buffer from SRAM during a line
    pixel_buffer pb(.clk(vga_clk), .reset(reset), .address(pb_address), .data_read(data_read), .read(pb_read), .ready(ready), .pixels(pixels), .hcounter(hcounter), .vcounter(vcounter)); 
     `endif
 
-   write_buffer wb(.clk(vga_clk), .reset(reset), .address(wb_address), .data_read(data_read), .ram_read(wb_read), .ram_ready(ready), .data_write(data_write), .ram_write(write), .erase(~BUTTON[0]), .cam_x(x), .cam_y(y), .cam_valid(cam_valid), .start(start_write), .clk_en(write_buf_clk_en));
-
+   // write buffer writes pixels to the SRAM at the end of the screen
+   write_buffer wb(.clk(vga_clk), .reset(reset), .address(wb_address), .data_read(data_read), .ram_read(wb_read), .ram_ready(ready), .data_write(data_write), .ram_write(write), .erase(erase), .cam_x(x), .cam_y(y), .cam_valid(cam_valid), .start(start_write), .clk_en(write_buf_clk_en));
 
     reg start_write;
     reg write_buf_clk_en;
     wire wb_read, pb_read;
     wire [17:0] pb_address;
     wire [17:0] wb_address;
-
-    /*
-    maybe interaction between this and the below is causing intermittent line drawing?
-    like if the start_write flag is missed by write_buffer
-    */
-
     
+    // start signal for capturing camera position and drawing next line
     always @(posedge vga_clk) 
-        start_write <= vcounter == 481 && hcounter == 0;
+        start_write <= vcounter == 480 && hcounter == 0;
 
-    // mux
+    // mux for SRAM address lines
     always @(posedge vga_clk) begin
         if( vcounter > 479 ) begin
             read <= wb_read;
